2994a112630fea045533ce94877208e3597fe3c0
GreyWindler <43067710+GreyWindler@users.noreply.github.com>
2018-09-29 21:08:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+from os import listdir
 
-import os,re
 
-migrations = '/Migrations/'
-current_dir = os.path.dirname(os.path.abspath(__file__))
+def search_file(drc, squery):
+    with open(drc) as f:
+        for line in f:
+            if squery in line:
+                print(drc)
+                return drc
+
+
+def search(dirs):
+    print('Введите искомую строку')
+    squery = input()
+
+    new_dirs = []
+    found_num = 0
+    for drc in dirs:
+        new_drc = search_file(drc, squery)
+        if new_drc:
+            found_num += 1
+            new_dirs.append(new_drc)
+    return new_dirs, found_num
+
 
 if __name__ == '__main__':
-	find = input("Введите строку:")
-	list_files = os.listdir(current_dir + migrations)
-	i=0
-	for file in list_files:
-		filename, file_extension = os.path.splitext(str(current_dir + migrations + file))
-		if "sql" in file_extension and find in open(filename + file_extension).read():
-			print(filename + file_extension)
-			i+=1
-	print (i)
-pass
+    dirs = ['\\'.join(['Migrations', d]) for d in listdir('Migrations') if d[-4:] == '.sql']
+    while True:
+        dirs, found_num = search(dirs)
+        print('Всего найдено: ', found_num)
