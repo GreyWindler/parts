cf8557d9f11b58fa7140e689a941d75108fa42d4
GreyWindler <43067710+GreyWindler@users.noreply.github.com>
2018-10-06 00:17:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from os import listdir
-
+import os.path
 
 def search_file(drc, squery):
     with open(drc) as f:
@@ -14,17 +14,16 @@ def search(dirs):
     squery = input()
 
     new_dirs = []
-    found_num = 0
     for drc in dirs:
         new_drc = search_file(drc, squery)
         if new_drc:
-            found_num += 1
             new_dirs.append(new_drc)
-    return new_dirs, found_num
+    return new_dirs
 
 
 if __name__ == '__main__':
-    dirs = ['\\'.join(['Migrations', d]) for d in listdir('Migrations') if d[-4:] == '.sql']
+    dirs = [os.path('Migrations', d) for d in listdir('Migrations') if os.path.splitext(d)[-1] == '.sql']
     while True:
-        dirs, found_num = search(dirs)
-        print('Всего найдено: ', found_num)
+        dirs = search(dirs)
+        print('Всего найдено: %s' % len(dirs))
+        
